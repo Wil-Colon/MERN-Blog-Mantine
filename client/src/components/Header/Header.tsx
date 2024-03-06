f2541da7dee83c47b2ad0020e3dfd5c0c7b78d3f
Wilfredo <drastic87@gmail.com>
2024-03-05 22:05:44 -0500

    header wip

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -4,33 +4,54 @@ import DarkModeBtn from "../Buttons/DarkModeBtn/DarkModeBtn";
 import HeaderButtons from "../Buttons/HeaderButtons/HeaderButtons";
 import logo from "../../assets/images/logo.png";
 import { Link, useLocation } from "react-router-dom";
+import { IconSailboat } from "@tabler/icons-react";
 
 export default function Header() {
   const path = useLocation().pathname;
   console.log(path);
   return (
     <header className="header">
-      <Flex className="header__logo-container">
-        <Image className="logo" src={logo} />
+      <Flex gap="md" className="header__top">
+        <div className="top-container__left">
+          <div className="top-container__left-logo">
+            Gilberto <span>Angling</span>
+          </div>
+          <div className="top-container__left-text">
+            <span>Blog Posts: Num</span>
+
+            <span>Thought Posts: Num</span>
+          </div>
+        </div>
+
+        <div className="top-container__right">
+          <div className="top-container__right-text">
+            Admin Login{" "}
+            <span>
+              <IconSailboat />
+            </span>
+          </div>
+          <span>
+            <DarkModeBtn />
+          </span>
+        </div>
       </Flex>
+
       <Flex
         mih={50}
-        justify="flex-end"
         align="center"
         direction="row"
         wrap="nowrap"
-        className="header__btns"
+        className="header__bottom"
       >
-        <Link to="/">
+        <Link to="/" className="link-item">
           <HeaderButtons>Home</HeaderButtons>
         </Link>
-        <Link to="/about">
+        <Link to="/about" className="link-item">
           <HeaderButtons>About</HeaderButtons>
         </Link>
-        <Link to="/blogs">
+        <Link to="/blogs" className="link-item">
           <HeaderButtons>Blogs</HeaderButtons>
         </Link>
-        <DarkModeBtn />
       </Flex>
     </header>
   );
