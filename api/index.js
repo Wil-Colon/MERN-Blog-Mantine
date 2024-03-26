91e409293114004f7c1e3ccd15bfc224a29e8440
Wilfredo <drastic87@gmail.com>
2024-03-26 19:12:15 -0400

    create db, usermodel

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
-import express from "express";
-
+const express = require("express");
 const app = express();
+const db = require("./db");
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log("Server running in port 3000");
+db();
+
+app.listen(port, () => {
+  console.log(`Server running in port ${port}`);
 });
